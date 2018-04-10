7a7046ea67e8f70cf83f203821bf7243bea6e526
Unknown <lukaswiedemann@gmail.com>
2018-04-10 13:21:05 +0200

    Created Basic Menue
    Menue at the bottom created. Click opens and hides a placeholder

---
+++ Assets/Scripts/MenueControll.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenueControll : MonoBehaviour {
+
+    public enum Screen {
+        NONE = 0,
+        MAP,
+        RESEARCH,
+        PRODUCTION_BOOST,
+        BLACK_MARKET,
+        OPTIONS
+    };
+
+    Screen enabledScreen;
+
+    // Use this for initialization
+    void Start () {
+        enabledScreen = Screen.NONE;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenMenue(Screen screenType) {
+        switch (screenType) {
+            case Screen.MAP:
+                if (enabledScreen == Screen.MAP) {
+                    transform.localScale = new Vector3(1, 1, 1);
+                    enabledScreen = Screen.NONE;
+                } else {
+                    transform.localScale = new Vector3(1, 7, 1);
+                    enabledScreen = Screen.MAP;
+                }
+                break;
+            default:
+                break;
+        }
+        
+        //transform.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/UIInteraction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInteraction : MonoBehaviour {
+
+    private MenueControll menueController;
+
+    // Use this for initialization
+    void Start () {
+
+        menueController = GameObject.Find("BackgroundBuyMenue").GetComponent<MenueControll>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenWorldMap() {
+        menueController.OpenMenue(MenueControll.Screen.MAP);
+        Debug.Log("OpenWorldMap");
+    }
+
+    public void OpenResearch() {
+        Debug.Log("OpenResearch");
+    }
+
+    public void OpenProductionBoost() {
+        Debug.Log("OpenProductionBoost");
+    }
+
+    public void OpenBlackMarket() {
+        Debug.Log("OpenBlackMarket");
+    }
+
+    public void OpenOptions() {
+        Debug.Log("OpenOptions");
+    }
+
+
+}
