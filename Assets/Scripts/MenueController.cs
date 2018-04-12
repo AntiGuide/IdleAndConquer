fdbed55010255d432fcb8d53cbe91c44343940e0
Unknown <lukaswiedemann@gmail.com>
2018-04-12 11:42:30 +0200

    Fixed Buy-Menue for scrolling and added ScrollView
    Buy-Menue had problems with different aspect ratios. Scaling of the canvas was not considered.

--- Assets/Scripts/MenueController.cs
+++ Assets/Scripts/MenueController.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class MenueController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
 
     public static GameObject itemBeingDragged;
-
+    
+    public float menueExpandedHeight = 0.8f;
+    public float menueExbandTriggerHeight = 0.6f;
     public enum MenueCategory
     {
         NONE = 0,
@@ -18,25 +20,33 @@ public class MenueController : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     };
 
     MenueCategory enabledScreen;
+    private RectTransform canvasRectTransform;
     float canvasHeight;
     float startYMenue;
-    private float lerpJourneyLength;
-    private float lerpStartTime;
-    private Vector3 startMarker;
-    private Vector3 endMarker;
-    private float lerpSpeed;
-    private bool menueLerping;
+    float lerpJourneyLength;
+    float lerpStartTime;
+    Vector3 startMarker;
+    Vector3 endMarker;
+    public float lerpSpeed = 1000f;
+    bool menueLerping;
+
+    private void Awake() {
+        //Set screen size for Standalone
+#if UNITY_STANDALONE
+         Screen.SetResolution(576, 1024, false);
+         Screen.fullScreen = false;
+#endif
+    }
 
     public void OnBeginDrag(PointerEventData eventData) {
         itemBeingDragged = gameObject;
-        
     }
 
     public void OnDrag(PointerEventData eventData) {
         float y = eventData.position.y;
 
-        if (y > canvasHeight * 0.8f) {
-            y = canvasHeight * 0.8f;
+        if (y > canvasHeight * menueExpandedHeight) {
+            y = canvasHeight * menueExpandedHeight;
         } else if (y < startYMenue) {
             y = startYMenue;
         }
@@ -46,8 +56,9 @@ public class MenueController : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     public void OnEndDrag(PointerEventData eventData) {
         itemBeingDragged = null;
         float y = transform.position.y;
-        if (y > canvasHeight * 0.6f) {
-            y = canvasHeight * 0.8f;
+        Debug.Log(y);
+        if (y > canvasHeight * menueExbandTriggerHeight) {
+            y = canvasHeight * menueExpandedHeight;
             enabledScreen = MenueCategory.MAP;
         } else {
             y = startYMenue;
@@ -57,19 +68,34 @@ public class MenueController : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         lerpJourneyLength = Vector3.Distance(startMarker, endMarker);
         menueLerping = true;
         lerpStartTime = Time.time;
-        //transform.position = new Vector3(transform.position.x, y, 0);
     }
 
     // Use this for initialization
     void Start () {
         enabledScreen = MenueCategory.NONE;
-        canvasHeight = GameObject.Find("/Canvas").GetComponent<RectTransform>().rect.height;
+        canvasRectTransform = GameObject.Find("/Canvas").GetComponent<RectTransform>();
+        canvasHeight = canvasRectTransform.rect.height * canvasRectTransform.localScale.y;
         startYMenue = transform.position.y;
-        lerpSpeed = 1000.0f;
+        
+        /*Vector3[] tmpVec = new Vector3[4];
+        canvasRectTransform.GetLocalCorners(tmpVec);
+        for (int i = 0; i < 4; i++) {
+            Debug.Log(tmpVec[i].x + " | " + tmpVec[i].y);
+
+        }
+        tmpVec = new Vector3[4];
+        canvasRectTransform.GetWorldCorners(tmpVec);
+        for (int i = 0; i < 4; i++) {
+            Debug.Log(tmpVec[i].x + " | " + tmpVec[i].y);
+
+        }
+        
+        DrawLine(new Vector3(0f,111f,0f), new Vector3(300f, 111f, 0f), Color.red,20.0f);*/
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
         if (menueLerping && lerpJourneyLength == 0f) {
             menueLerping = false;
         }
@@ -95,8 +121,8 @@ public class MenueController : MonoBehaviour, IBeginDragHandler, IDragHandler, I
             case MenueCategory.MAP:
                 if (enabledScreen == MenueCategory.MAP) {
                     float y = transform.position.y;
-                    if (y > canvasHeight * 0.8f) {
-                        y = canvasHeight * 0.8f;
+                    if (y > canvasHeight * menueExpandedHeight) {
+                        y = canvasHeight * menueExpandedHeight;
                     }
                     transform.position = new Vector3(transform.position.x, y, 0);
                     enabledScreen = MenueCategory.NONE;
@@ -110,4 +136,26 @@ public class MenueController : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
         //transform.localPosition = Vector3.zero;
     }
+
+
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f) {
+        
+        GameObject myLine = new GameObject("Line", typeof(LineRenderer));
+        myLine.transform.parent = GameObject.Find("/Canvas").transform;
+        myLine.transform.position = start;
+        //myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.startColor = color;
+        lr.endColor = color;
+        lr.startWidth = 1.0f;
+        lr.endWidth = 1.0f;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
+
+
+
 }
