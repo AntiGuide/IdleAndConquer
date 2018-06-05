7fc93495f7807b0855e70be5bac26f56cc0ccb1a
Unknown <lukaswiedemann@gmail.com>
2018-06-05 13:18:25 +0200

    Refactored PowerPlant.cs

--- Assets/Scripts/Base/PowerPlant.cs
+++ Assets/Scripts/Base/PowerPlant.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 
-public class PowerPlant : MonoBehaviour
-{
-    private EnergyManagement energyManagement;
-    public float energyProduction;
+/// <summary>
+/// Script to be attached to the power plant
+/// </summary>
+public class PowerPlant : MonoBehaviour {
+    /// <summary>The energy production for an object</summary>
+    public float EnergyProduction;
 
-    private void Start() {
-        energyManagement = GameObject.Find("/Canvas/BackgroundSideStrip").GetComponent<EnergyManagement>();
+    /// <summary>The reference to the energy manager to add the energy</summary>
+    private EnergyManagement energyManagement;
+
+    /// <summary>Adds the energy upon initialization (after build confirmed).</summary>
+    public void InitializeBuilt() {
+        this.energyManagement.addEnergy(this.EnergyProduction);
     }
 
-    public void InitializeBuilt() {
-        energyManagement.addEnergy(energyProduction);
+    /// <summary>Runs one time when object is instanciated</summary>
+    private void Start() {
+        this.energyManagement = GameObject.Find("/Canvas/BackgroundSideStrip").GetComponent<EnergyManagement>();
     }
 }
