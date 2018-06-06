303237e5d7c18a36c07135a7f28ba6e0f3ded5a6
Unknown <lukaswiedemann@gmail.com>
2018-06-06 11:14:17 +0200

    Basic pause recognition implemented

--- Assets/Scripts/UI/AppPauseHandler.cs
+++ Assets/Scripts/UI/AppPauseHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,22 +14,40 @@ public class AppPauseHandler : MonoBehaviour {
     /// <summary>Time since login</summary>
     private float loginTimer = 0f;
 
+    /// <summary>The date and time the user paused the app</summary>
+    private DateTime exitTime;
+
     /// <summary>
     /// Triggers one time on application pause or unpause
     /// </summary>
     /// <param name="pauseStatus">True if game is paused</param>
     void OnApplicationPause(bool pauseStatus) {
-        if (pauseStatus) {
+        Debug.Log("OnApplicationPause(" + pauseStatus.ToString() + ")");
+        if (!pauseStatus) {
             loginTimer = 0f;
-            testText.text = "Willkommen";
-            //Player is back
+            if (exitTime.CompareTo(DateTime.MinValue) == 0) {
+                // First start/New start
+                testText.text = "Willkommen";
+            } else {
+                // Player back from pause
+                testText.text = "Willkommen" + Environment.NewLine + Math.Round((System.DateTime.Now - exitTime).TotalSeconds) + " Sekunden Abwesenheit";
+            }
+            // Compate Times
+            // Player is back
+        } else {
+            exitTime = System.DateTime.Now;
         }
     }
 
+    private void Start() {
+        // Set exitTime to PlayerPref Application Quit?
+    }
+
     private void Update() {
-        loginTimer += Time.deltaTime;
         if (loginTimer > 5f) {
             testText.text = "";
+        } else {
+            loginTimer += Time.deltaTime;
         }
     }
 }
