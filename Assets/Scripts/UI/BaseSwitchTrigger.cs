327ea7efcbb09d58156287af2343544cecf517f8
Unknown <lukaswiedemann@gmail.com>
2018-06-20 17:01:47 +0200

    Began work on base switching

---
+++ Assets/Scripts/UI/BaseSwitchTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to call base switching on player input
+/// </summary>
+public class BaseSwitchTrigger : MonoBehaviour {
+    /// <summary>The base switcher manager to call the switch in</summary>
+    public BaseSwitcher BaseSwitch;
+
+    /// <summary>
+    /// Switches the base after a button click
+    /// </summary>
+    /// <param name="isLeft">If true the base should be switched to the one on the left. If false its to the right</param>
+    public void OnClickBaseSwitch(bool isLeft) {
+        bool leftPossible = false;
+        bool rightPossible = false;
+
+        this.BaseSwitch.OnClickBaseSwitch(isLeft, out leftPossible, out rightPossible);
+
+        // WIP
+    }
+}
---
+++ Assets/Scripts/UI/BaseSwitcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to handle base switching on player input
+/// </summary>
+public class BaseSwitcher : MonoBehaviour {
+    /// <summary>An array of the base containers to enable/disable a base</summary>
+    public GameObject[] Bases;
+
+    /// <summary>An array of bools that indicate wether a base is enable/disable (indicator for array Bases)</summary>
+    public bool[] EnablesBases;
+
+    /// <summary>The index of the current enabled base</summary>
+    public int CurrentBase;
+
+    /// <summary>
+    /// Switches the base after a button click
+    /// </summary>
+    /// <param name="isLeft">If true the base should be switched to the one on the left. If false its to the right</param>
+    public void OnClickBaseSwitch(bool isLeft, out bool leftPossible, out bool rightPossible) {
+        if (isLeft && this.CurrentBase > 0) {
+            this.Bases[this.CurrentBase].SetActive(false);
+            this.CurrentBase--;
+            this.Bases[this.CurrentBase].SetActive(true);
+        } else if (this.CurrentBase < this.Bases.Length - 1) {
+            this.Bases[this.CurrentBase].SetActive(false);
+            this.CurrentBase++;
+            this.Bases[this.CurrentBase].SetActive(true);
+        }
+
+        this.CheckPossibilities(out leftPossible, out rightPossible);
+    }
+
+    /// <summary>
+    /// Checks if it is possible to move to a base in both directions
+    /// </summary>
+    /// <param name="leftPossible">Is left possible</param>
+    /// <param name="rightPossible">Is right possible</param>
+    public void CheckPossibilities(out bool leftPossible, out bool rightPossible) {
+        // Check if switches in the directions are possible
+        leftPossible = this.CurrentBase > 0;
+        rightPossible = this.CurrentBase < this.Bases.Length - 1;
+    }
+}
