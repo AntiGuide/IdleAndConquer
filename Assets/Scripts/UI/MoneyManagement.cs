ea2f05649fa3eefb5e048a72fe4373f7cddccb92
Unknown <lukaswiedemann@gmail.com>
2018-06-22 12:01:29 +0200

    Implemented money display lerp

--- Assets/Scripts/UI/MoneyManagement.cs
+++ Assets/Scripts/UI/MoneyManagement.cs
@@ -7,8 +7,23 @@ using UnityEngine.UI;
 
 /// <summary>Manages the money of the player</summary>
 public class MoneyManagement : MonoBehaviour {
+    /// <summary>The time the money takes to lerp to a specific value</summary>
+    public float LerpTimeStart = 1.0f;
+    
     /// <summary>Saves the amout of money the player has at the moment</summary>
     private static long money;
+    
+    /// <summary>How much seconds of the lerp are completed</summary>
+    private float lerpTimeDone = 0.0f;
+
+    /// <summary>The amount of money that the player lerps from</summary>
+    private long moneyAmountOld;
+
+    /// <summary>The amount of money that is shown at the moment</summary>
+    private long moneyAmountShown;
+
+    /// <summary>The amount of money that is goal of the lerp</summary>
+    private long moneyToLerpTo;
 
     /// <summary>
     /// Formats the money to german format
@@ -27,7 +42,7 @@ public class MoneyManagement : MonoBehaviour {
     /// <param name="moneyToAdd">The amount to add</param>
     public void AddMoney(long moneyToAdd) {
         money = money + moneyToAdd;
-        this.OutputMoney(money);
+        this.OutputMoney(money, true);
     }
 
     /// <summary>
@@ -38,7 +53,7 @@ public class MoneyManagement : MonoBehaviour {
     public bool SubMoney(long moneyToSub) {
         if (money >= moneyToSub) {
             money = money - moneyToSub;
-            this.OutputMoney(money);
+            this.OutputMoney(money, true);
             return true;
         } else {
             return false;
@@ -61,7 +76,7 @@ public class MoneyManagement : MonoBehaviour {
     public void SetMoney(long valueToSet) {
         if (valueToSet >= 0) {
             money = valueToSet;
-            this.OutputMoney(money);
+            this.OutputMoney(money, false);
         } else {
             throw new ArgumentException("Can not set a negative Dollar Value", "valueToSet");
         }
@@ -72,11 +87,29 @@ public class MoneyManagement : MonoBehaviour {
         this.SetMoney(20000);
     }
 
+    /// <summary>Called once per frame</summary>
+    private void Update() {
+        if (this.moneyToLerpTo != this.moneyAmountShown) {
+            this.lerpTimeDone += Time.deltaTime;
+            this.lerpTimeDone = Mathf.Min(this.LerpTimeStart, this.lerpTimeDone);
+            this.moneyAmountShown = (long)Mathf.Lerp(this.moneyAmountOld, this.moneyToLerpTo, this.lerpTimeDone / this.LerpTimeStart);
+            this.GetComponent<Text>().text = FormatMoney(this.moneyAmountShown);
+        }
+    }
+
     /// <summary>
     /// Display the money
     /// </summary>
     /// <param name="money">How much money to display</param>
-    private void OutputMoney(long money) {
-        GetComponent<Text>().text = FormatMoney(money);
+    private void OutputMoney(long money, bool lerpEffect) {
+        if (lerpEffect) {
+            this.moneyAmountOld = this.moneyAmountShown;
+            this.moneyToLerpTo = money;
+            this.lerpTimeDone = 0.0f;
+        } else {
+            this.GetComponent<Text>().text = FormatMoney(money);
+            this.moneyAmountShown = money;
+            this.moneyToLerpTo = money;
+        }
     }
 }
