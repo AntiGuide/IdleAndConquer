a347213cd683dd4eb9fb11d42fbc86471083300b
Unknown <lukaswiedemann@gmail.com>
2018-07-02 11:25:33 +0200

    Implemented Tab Switching

---
+++ Assets/Scripts/UI/TabSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabSwitcher : MonoBehaviour {
+    public GameObject[] tabs;
+    private int aktTabID;
+
+    public void OnClickTab(int tabID) {
+        if (tabID != aktTabID) {
+            tabs[aktTabID].SetActive(false);
+            aktTabID = tabID;
+            tabs[tabID].SetActive(true);
+        }
+    }
+}
