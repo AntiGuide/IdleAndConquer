6efbdb5a25729a35c7330b599e3c02745f746dcc
Unknown <lukaswiedemann@protonmail.com>
2018-07-05 20:49:21 +0200

    Final before presentation

--- Assets/Scripts/UI/EnergyManagement.cs
+++ Assets/Scripts/UI/EnergyManagement.cs
@@ -8,14 +8,24 @@ public class EnergyManagement : MonoBehaviour {
     public float maxEnergy = 100;
     public float minEnergy = 0;
     public float redStep = 24f;
+    public float blinkTime = 0.25f;
     private float curEnergy = 0;
     private Color prevColor;
+    private bool blinking = false;
+    private float blinkTimeAkt;
+    private bool isRed = false;
 
     public void OutputEnergy(float curEnergy, float maxEnergy, float minEnergy) {
         this.curEnergy = curEnergy;
         this.maxEnergy = maxEnergy;
         this.minEnergy = minEnergy;
         this.curEnergy = this.curEnergy <= this.redStep - 1 ? this.redStep - 1 : this.curEnergy;
+        if (this.curEnergy <= this.redStep - 1) {
+            this.blinking = true;
+            blinkTimeAkt = blinkTime;
+        } else {
+            isRed = false;
+        }
         this.Bar.fillAmount = this.curEnergy / (this.maxEnergy - this.minEnergy);
     }
 
@@ -26,6 +36,11 @@ public class EnergyManagement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        gameObject.GetComponent<Image>().color = this.curEnergy < this.redStep ? Color.red : this.prevColor;
+        blinkTimeAkt -= Time.deltaTime;
+        if (blinking && blinkTimeAkt <= 0) {
+            isRed = !isRed;
+            blinkTimeAkt = blinkTime;
+        }
+        gameObject.GetComponent<Image>().color = isRed ? Color.red : this.prevColor;
     }
 }
