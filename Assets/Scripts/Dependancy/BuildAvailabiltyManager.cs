117be668e100c5eaa07c283d7e8d8b319548f356
Unknown <lukaswiedemann@protonmail.com>
2018-08-08 16:12:52 +0200

    Minor adjustments

--- Assets/Scripts/Dependancy/BuildAvailabiltyManager.cs
+++ Assets/Scripts/Dependancy/BuildAvailabiltyManager.cs
@@ -1,5 +1,4 @@
-﻿using UnityEditorInternal;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BuildAvailabiltyManager : MonoBehaviour {
     [SerializeField] private BuildButtonManager[] buildButtonManagers;
